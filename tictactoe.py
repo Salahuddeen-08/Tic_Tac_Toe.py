103e4a4d30614ef5281a3cd1a72d59fb96b7285f
Salahuddeen Rasheed <salahuddeenr2008@gmail.com>
2021-08-29 20:10:33 +0400

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,73 @@
+from tkinter import *
+import tkinter.messagebox
+tk = Tk()
+tk.title("Tic Tac Toe")
+
+playera = StringVar()
+playerb = StringVar()
+p1 = StringVar()
+p2 = StringVar()
+
+player1_name = Entry(tk, textvariable=p1, bd=5)
+player1_name.grid(row=1, column=1, columnspan=8)
+player2_name = Entry(tk, textvariable=p2, bd=5)
+player2_name.grid(row=2, column=1, columnspan=8)
+
+bclick = True
+flag = 0
+
+def disableButton():
+    button1.configure(state=DISABLED)
+    button2.configure(state=DISABLED)
+    button3.configure(state=DISABLED)
+    button4.configure(state=DISABLED)
+    button5.configure(state=DISABLED)
+    button6.configure(state=DISABLED)
+    button7.configure(state=DISABLED)
+    button8.configure(state=DISABLED)
+    button9.configure(state=DISABLED)
+
+
+
+
+
+
+
+
+buttons = StringVar()
+
+label = Label( tk, text="Player 1:", font='Times 20 bold', bg='white', fg='black', height=1, width=8)
+label.grid(row=1, column=0)
+
+
+label = Label( tk, text="Player 2:", font='Times 20 bold', bg='white', fg='black', height=1, width=8)
+label.grid(row=2, column=0)
+
+button1 = Button(tk, text=" ", font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button1))
+button1.grid(row=3, column=0)
+
+button2 = Button(tk, text=' ', font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button2))
+button2.grid(row=3, column=1)
+
+button3 = Button(tk, text=' ',font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button3))
+button3.grid(row=3, column=2)
+
+button4 = Button(tk, text=' ', font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button4))
+button4.grid(row=4, column=0)
+
+button5 = Button(tk, text=' ', font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button5))
+button5.grid(row=4, column=1)
+
+button6 = Button(tk, text=' ', font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button6))
+button6.grid(row=4, column=2)
+
+button7 = Button(tk, text=' ', font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button7))
+button7.grid(row=5, column=0)
+
+button8 = Button(tk, text=' ', font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button8))
+button8.grid(row=5, column=1)
+
+button9 = Button(tk, text=' ', font='Times 30 bold', bg='lime', fg='white', height=4, width=8, command=lambda: btnClick(button9))
+button9.grid(row=5, column=2)
+
+tk.mainloop()
